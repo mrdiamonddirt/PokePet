c4110073ae9941b3c42114a20b3df4583302e4db
RaeesaParker <raeesaparker101@gmail.com>
2022-10-19 15:36:05 +0100

    Add code for progress bars

--- app.js
+++ app.js
@@ -49,3 +49,64 @@ if (diplayedDivID == 1) {
     createbtn()
 
 }
+
+
+
+// ================================
+// ----- Create Property Bars 
+
+/*
+	<div class="mini-hero-progress-boxes">
+		<div id="id-progress-1" class="mini-hero-progress-bar"></div>
+		<div class="mini-hero-progress-perc" ><p>100%</p></div>
+	</div>
+
+
+
+
+#id-progress-1{
+	background: linear-gradient(to right, var(--gold) 100%, var(--gold) 100%, var(--darkBlue) 50%);
+}
+*/
+
+function createPropertyBars(health, hunger, sleep, happiness){
+
+	// Create array which hold property names
+	let propertiesArray = ["Health", "Hunger", "Sleep", "Happiness"]
+
+	// Create div to hold property bar
+	const propertiesDiv = document.createElement('div')
+
+	// Create paragraph tag to hold property name => Append to the new progress div
+	const newPara = document.createElement('p')
+	propertiesDiv.appendChild(newPara.appendChild(document.createTextNode(`${propertiesArray[0]}`)))
+
+	// Create the div to hold the property bar and paratag
+	const barBoxDiv = document.createElement('div')
+	barBoxDiv.classList.add("barBox-div");
+	propertiesDiv.appendChild(barBoxDiv)
+
+	// Create the div to hold the actual bar
+	const barDiv = document.createElement('div')
+	barDiv.classList.add("bar-div");
+	barDiv.setAttribute('id',`properties-bar-1`);
+	barBoxDiv.appendChild(barDiv)
+
+	// Create the div to hold the paragraph tag with the percentage value
+	const propertyValueDiv = document.createElement('div')
+	propertyValueDiv.classList.add("property-perc");
+	const propertyValuePara = document.createElement('p')
+	propertyValueDiv.appendChild(propertyValuePara.appendChild(document.createTextNode(`100`)))
+
+	barBoxDiv.appendChild(propertyValueDiv)
+
+
+	// Append progress div to main div
+	mainDiv.appendChild(propertiesDiv);
+
+}
+
+createPropertyBars()
+
+
+
