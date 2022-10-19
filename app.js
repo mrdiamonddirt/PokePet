9d317e6ab6cf4be319d6e0e7cf1fccc5b5768f02
RowDog <mrdiamonddirt@gmail.com>
2022-10-19 16:25:55 +0100

    bars

--- app.js
+++ app.js
@@ -101,6 +101,52 @@ if (diplayedDivID == 1) {
   createbtn();
 }
 
+// ----- Create Property Bars 
+
+function createPropertyBars(health, hunger, sleep, happiness){
+
+	// Create array which hold property names
+	let propertiesArray = ["Health", "Hunger", "Sleep", "Happiness"]
+
+		for (let i=0 ; i<propertiesArray.length ; i++ ){
+
+		// Create div to hold property bar
+		const propertiesDiv = document.createElement('div')
+
+		// Create paragraph tag to hold property name => Append to the new progress div
+		const newPara = document.createElement('p')
+		propertiesDiv.appendChild(newPara.appendChild(document.createTextNode(`${propertiesArray[i]}`)))
+
+		// Create the div to hold the property bar and paratag
+		const barBoxDiv = document.createElement('div')
+		barBoxDiv.classList.add("barBox-div");
+		propertiesDiv.appendChild(barBoxDiv)
+
+		// Create the div to hold the actual bar
+		const barDiv = document.createElement('div')
+		barDiv.classList.add("bar-div");
+		barDiv.setAttribute('id',`properties-bar-1`);
+		barBoxDiv.appendChild(barDiv)
+
+		// Create the div to hold the paragraph tag with the percentage value
+		const propertyValueDiv = document.createElement('div')
+		propertyValueDiv.classList.add("property-perc");
+		const propertyValuePara = document.createElement('p')
+		propertyValueDiv.appendChild(propertyValuePara.appendChild(document.createTextNode(`100`)))
+
+		barBoxDiv.appendChild(propertyValueDiv)
+
+		// Add styling to the bar depending on the value of the property
+		barDiv.style.background= "linear-gradient(to right, green 75%, green 90%, blue 1%)";
+
+		// Append progress div to main div
+		mainDiv.appendChild(propertiesDiv);
+	}
+
+}
+
+createPropertyBars()
+
 // Create New Pet give it a name and mood name and mood
 let charmander = new Charmander('dave','happy')
 charmander.playGame()
