1ef16f107e71354c46ddd1aaf6db7776f5afa6dc
RaeesaParker <raeesaparker101@gmail.com>
2022-10-19 13:46:10 +0100

    Add class for pet

---
+++ app.js
@@ -0,0 +1,16 @@
+// ================================
+// ----- Classes
+// ================================
+
+
+// Create class to store pet
+class Pet {
+	constructor(name, mood){
+		this.name = name;
+		this.mood = mood;
+		this.health = 100;
+		this.hunger = 100;
+		this.sleep = 100;
+		this.happiness = 100;
+	}
+}
