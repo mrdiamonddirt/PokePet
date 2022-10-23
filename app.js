e0923d8cc0ddcd5155a3087393174e7fe3604bff
RaeesaParker <raeesaparker101@gmail.com>
2022-10-23 23:26:32 +0100

    Added loop to bgmusic

--- app.js
+++ app.js
@@ -147,14 +147,16 @@ var click = new Audio("sounds/button-click.mp3");
 var healsound = new Audio("sounds/poke-recovery.mp3");
 var bgmusic = new Audio("sounds/pallet-Town8bit.mp3");
 
-//  Start background music 
-bgmusic.play()
-bgmusic.loop = true
+//  Start background music (may not work in browsers)
+bgmusic.play();
+bgmusic.loop = true;
 
+// Automatically start music in browsers
 window.addEventListener("DOMContentLoaded", event => {
   const audio = bgmusic
   audio.volume = 0.2;
   audio.play();
+  audio.loop = true;
 });
 
 // Set up mute button
